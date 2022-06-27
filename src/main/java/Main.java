67bde7447f0274ecc2662d668ed02dd342100298
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-06-26 20:02:38 -0400

    SumofInput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.*;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int sum= 0;
+        System.out.print("Enter a number");
+        int n = sc.nextInt();
 
+
+        for (i = 1; i <= n; i++) {
+            sum= i+sum;
+
+
+
+        }
+
+        System.out.println("Total= " +sum);
     }
 }
+
